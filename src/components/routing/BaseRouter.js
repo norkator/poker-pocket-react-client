2f61aa223dada62537d4b69df2f1d627dbb2d195
norkator <martin.kankaanranta@hotmail.com>
2024-12-16 20:52:22 +0200

    navigation based on game selection

--- src/components/routing/BaseRouter.js
+++ src/components/routing/BaseRouter.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import MainLayout from '@/layouts/MainLayout';
 import HoldemPage from '@/pages/HoldemPage';
 import GamesPage from '@/pages/GamesPage';
+import FiveCardDrawPage from '@/pages/FiveCardDrawPage';
 
 const AppRoute = ({ component: Component, layout: Layout, ...rest }) => {
   return (
@@ -17,6 +18,11 @@ const BaseRouter = () => {
     <Routes>
       <Route path="/" element={<AppRoute component={HoldemPage} layout={MainLayout} />} />
       <Route path="/games" element={<AppRoute component={GamesPage} layout={MainLayout} />} />
+      <Route path="/holdem" element={<AppRoute component={HoldemPage} layout={MainLayout} />} />
+      <Route
+        path="/fivecarddraw"
+        element={<AppRoute component={FiveCardDrawPage} layout={MainLayout} />}
+      />
     </Routes>
   );
 };
