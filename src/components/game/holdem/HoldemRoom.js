eac5196890c5a7ce38b8744aff003683c2d5511b
norkator <martin.kankaanranta@hotmail.com>
2024-12-16 21:51:31 +0200

    five card draw has own room, table and seat slot

--- src/components/game/holdem/HoldemRoom.js
+++ src/components/game/holdem/HoldemRoom.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import RoomStatus from '@/components/game/RoomStatus';
-import RoomTable from '@/components/game/RoomTable';
+import HoldemTable from '@/components/game/holdem/HoldemTable';
 import BoardCards from '@/components/game/BoardCards';
 import TurnControl from '@/components/game/TurnControl';
 
@@ -9,11 +9,11 @@ const HoldemRoom = () => {
     <>
       <RoomStatus />
       {/* <!-- Poker table --> */}
-      <RoomTable>
+      <HoldemTable>
         <div style={{ marginTop: '15px', marginLeft: '20px' }}>
           <BoardCards />
         </div>
-      </RoomTable>
+      </HoldemTable>
       <TurnControl />
     </>
   );
