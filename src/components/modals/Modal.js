91812f271355cb3fb09fd623e647b09338c84236
norkator <martin.kankaanranta@hotmail.com>
2024-12-23 22:34:43 +0200

    bootstrap upgrade related modal change

--- src/components/modals/Modal.js
+++ src/components/modals/Modal.js
@@ -12,7 +12,7 @@ const ModalWrapper = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  z-index: 101;
+  z-index: 1050;
   background-color: rgba(0, 0, 0, 0.5);
 `;
 
@@ -25,37 +25,24 @@ const Modal = ({
   showHeader = true,
   showFooter = true,
 }) => {
+  const handleBackdropClick = (e) => {
+    // if (e.target.id === 'wrapper') {
+    //   onClose();
+    // }
+  };
   return ReactDOM.createPortal(
-    <ModalWrapper
-      id="wrapper"
-      // onClick={(e) => {
-      //   if (e.target.id === 'wrapper') {
-      //     onClose();
-      //   }
-      // }}
-    >
-      <div className="modal-dialog" role="document">
+    <ModalWrapper id="wrapper" onClick={handleBackdropClick}>
+      <div className="modal modal-dialog modal-dialog-centered" role="document">
+        {' '}
         <div className="modal-content">
-          {showHeader ? (
+          {showHeader && (
             <div className="modal-header">
               <h5 className="modal-title">{headingText}</h5>
-              <button
-                type="button"
-                className="close"
-                onClick={onClose}
-                data-dismiss="modal"
-                aria-label="Close"
-              >
-                <span aria-hidden="true">&times;</span>
-              </button>
+              <button type="button" className="btn-close" onClick={onClose} aria-label="Close" />
             </div>
-          ) : (
-            ''
           )}
           <div className="modal-body">
-            {children ? (
-              children
-            ) : (
+            {children || (
               <div>
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis error
                 aspernatur vel fugiat quisquam aut tempore, consequatur quo. Neque officiis magni
@@ -63,19 +50,12 @@ const Modal = ({
               </div>
             )}
           </div>
-          {showFooter ? (
+          {showFooter && (
             <div className="modal-footer">
-              <button
-                type="button"
-                className="btn btn-secondary"
-                onClick={onBtnClicked}
-                data-dismiss="modal"
-              >
+              <button type="button" className="btn btn-secondary" onClick={onBtnClicked}>
                 {btnText}
               </button>
             </div>
-          ) : (
-            ''
           )}
         </div>
       </div>
@@ -85,12 +65,17 @@ const Modal = ({
 };
 
 Modal.propTypes = {
-  showHeader: PropTypes.bool.isRequired,
-  showFooter: PropTypes.bool.isRequired,
+  showHeader: PropTypes.bool,
+  showFooter: PropTypes.bool,
   headingText: PropTypes.string,
   btnText: PropTypes.string,
   onClose: PropTypes.func.isRequired,
   onBtnClicked: PropTypes.func.isRequired,
+};
+
+Modal.defaultProps = {
+  showHeader: true,
+  showFooter: true,
 };
 
 const initialModalData = {
