f2295db5e280149c98999344fc706c631fe446fc
norkator <martin.kankaanranta@hotmail.com>
2024-12-29 10:20:41 +0200

    change planned api urls to be accessible from /api

--- src/clientConfig.js
+++ src/clientConfig.js
@@ -2,9 +2,9 @@ const config = {
   isProduction: process.env.NODE_ENV === 'production',
   socketURI:
     process.env.NODE_ENV === 'production'
-      ? 'wss://pokerpocket-wss.nitramite.com'
+      ? 'wss://pokerpocket.nitramite.com/api'
       : process.env.NODE_ENV === 'staging'
-        ? 'wss://pokerpocket-staging-wss.nitramite.com'
+        ? 'wss://pokerpocket-staging.nitramite.com/api'
         : 'ws://localhost:8000',
 };
 
