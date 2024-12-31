5f1b8f43c59586d2b7428f1c1cb6d9979204d702
norkator <martin.kankaanranta@hotmail.com>
2024-12-31 20:46:16 +0200

    overlay to fcd

--- src/components/game/fiveCardDraw/FiveCardDrawRoom.js
+++ src/components/game/fiveCardDraw/FiveCardDrawRoom.js
@@ -3,19 +3,25 @@ import RoomStatus from '@/components/game/RoomStatus';
 import TurnControl from '@/components/game/TurnControl';
 import FiveCardDrawTable from '@/components/game/fiveCardDraw/FiveCardDrawTable';
 import BoardCards from '@/components/game/BoardCards';
+import Overlay from '@/components/Overlay';
 
 const FiveCardDrawRoom = () => {
   return (
-    <>
-      <RoomStatus />
-      {/* <!-- Poker table --> */}
-      <FiveCardDrawTable>
-        <div style={{ marginTop: '15px', marginLeft: '20px' }}>
-          <BoardCards />
-        </div>
-      </FiveCardDrawTable>
-      <TurnControl />
-    </>
+    <Overlay>
+      {(showOverlay) => (
+        <>
+          <RoomStatus />
+          {/* <!-- Poker table --> */}
+          <FiveCardDrawTable>
+            <div style={{ marginTop: '15px', marginLeft: '20px' }}>
+              <BoardCards />
+            </div>
+          </FiveCardDrawTable>
+          <TurnControl />
+          {/*<button onClick={() => showOverlay('🎉 +10 XP WIN STREAK!')}>Overlay</button> */}
+        </>
+      )}
+    </Overlay>
   );
 };
 
