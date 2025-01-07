c0e384415dedfdcf35a0912cb7cd049daa480e3e
norkator <martin.kankaanranta@hotmail.com>
2025-01-07 20:24:56 +0200

    craco config to set ANALYTICS_ID for index html from env variable

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,22 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   webpack: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+    configure: (webpackConfig) => {
+      const htmlPlugin = webpackConfig.plugins.find(
+        (plugin) => plugin instanceof HtmlWebpackPlugin
+      );
+
+      if (htmlPlugin) {
+        htmlPlugin.options.ANALYTICS_ID =
+          process.env.ANALYTICS_ID === undefined ? 'analytics-empty-id' : process.env.ANALYTICS_ID;
+      }
+
+      return webpackConfig;
     },
   },
 };
