f98efbabb30ab5b1f8af8e88609c7347439585d8
Martin Kankaanranta <martin.kankaanranta@hotmail.com>
2025-01-26 17:51:00 +0200

    fix wrong name in service worker registration

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -15,7 +15,7 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/serviceWorker.js`;
 
       if (isLocalhost) {
         checkValidServiceWorker(swUrl, config);
